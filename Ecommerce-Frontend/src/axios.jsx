f7eb9887f3736d41a6fea7f39c8325eb12c1201a
IshitaGujarathi <ishitagujarathi07@gmail.com>
2026-07-14 20:26:54 +0530

    Fix axios API instance

--- Ecommerce-Frontend/src/axios.jsx
+++ Ecommerce-Frontend/src/axios.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-axios.create({
+const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
+
 delete API.defaults.headers.common["Authorization"];
+
 export default API;
